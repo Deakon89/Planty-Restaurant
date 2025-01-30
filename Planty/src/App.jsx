2f4f0822723582535fd0acf1b17d965da93ebfd4
Deakon89 <franck.devw@gmail.com>
2025-01-30 12:24:49 +0100

    first try component
    navbar,card,layout

--- Planty/src/App.jsx
+++ Planty/src/App.jsx
@@ -1,12 +1,38 @@
-
-import './App.css'
+import NavBar from './component/navbar'
+import Card from './component/card.jsx' 
 
 function App() {
   
-
   return (
     <>
-  
+      <div className="container-fluid px-0">
+        <NavBar/>
+      </div>
+      <div className="container-fluid text-center bg-success-subtle">
+        <div className="row">
+          <div className="col">
+            <Card></Card>
+          </div>
+          <div className="col">
+          <Card></Card>
+          </div>
+          <div className="col">
+          <Card></Card>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <Card></Card>
+          </div>
+          <div className="col">
+          <Card></Card>
+          </div>
+          <div className="col">
+          <Card></Card>
+          </div>
+        </div>
+        
+      </div>
     </>
   )
 }
---
+++ Planty/src/component/navbar.jsx
@@ -0,0 +1,12 @@
+
+function NavBar() {
+    return (
+        <nav className="navbar bg-body-tertiary py-0">
+            <div className="container-fluid w bg-success">
+                <span className="navbar-brand mb-0 h1 text-warning">Planty</span>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
