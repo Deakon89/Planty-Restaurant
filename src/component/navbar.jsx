c89ed57f205e360ce840e5b00fa78dc15e3149f9
Deakon89 <franck.devw@gmail.com>
2025-02-11 15:23:00 +0100

    update

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -4,7 +4,7 @@ function NavBar() {
         <nav className="navbar sticky-top bg-body-tertiary py-0">
             <div className="container-fluid w bg-success">
                 <span className="navbar-brand mb-0 h1 text-warning">
-                    <a href="#"><img className='img-fluid' id="logo" src="/src/assets/logo_bg_removed.png.png" alt="Planty" /></a>
+                    <a href="#"><img className='img-fluid' id="logo" src="/dist/assets/logo_bg_removed.png.png" alt="Planty" /></a>
                 </span>
                 <a className='lime' href="#"><img src="/src/assets/lime.png" alt="lime" /></a>
             </div>
