f770f1b3fa6770ef6f78674da4ceccc5ea49f8f0
Deakon89 <franck.devw@gmail.com>
2025-02-11 23:06:27 +0100

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
     <App/>
     </BrowserRouter> 
   </StrictMode>
