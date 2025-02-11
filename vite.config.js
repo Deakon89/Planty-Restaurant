e0aae6640396b37a8dea38eecf21a20564843802
Deakon89 <franck.devw@gmail.com>
2025-02-11 21:54:56 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    mimeType: {
-      'js': 'text/javascript',
-      'jsx': 'text/javascript',
-    }
-  },
   resolve: {
     extensions: ['.js', '.jsx']
   }
