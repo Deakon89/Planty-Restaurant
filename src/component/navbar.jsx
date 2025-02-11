90ee70edc95e3d5a40716ad6840a66334e014bda
Deakon89 <franck.devw@gmail.com>
2025-02-11 15:23:42 +0100

    Update navbar.jsx

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
                 <span className="navbar-brand mb-0 h1 text-warning">
                     <a href="#"><img className='img-fluid' id="logo" src="/dist/assets/logo_bg_removed.png.png" alt="Planty" /></a>
                 </span>
-                <a className='lime' href="#"><img src="/src/assets/lime.png" alt="lime" /></a>
+                <a className='lime' href="#"><img src="/dist/assets/lime.png" alt="lime" /></a>
             </div>
         </nav>
     )
